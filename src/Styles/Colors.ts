a9f596eb8ac7aa907761320b05862f9612373ccd
Uğur Eren <contact@ugureren.net>
2023-04-27 20:22:17 +0300

    Theme colors

--- src/Styles/Colors.ts
+++ src/Styles/Colors.ts
@@ -1,31 +1,63 @@
 export const LightThemeColors = {
   colors: {
     transparent: 'transparent',
-    primary: '#8775FF',
-    accent: '#FFBE28',
-    background: '#F7F8FA',
-    backgroundSecondary: '#F5F5F5',
-    success: '#74D0AC',
-    error: '#ff4747',
+
+    primary: '#FA8057',
+    background: '#F0F0F0',
+    backgroundSecondary: '#FFFFFF',
+    surface: '#FAFAFA',
+
     text: '#242424',
-    textLight: '#272727',
+    textLight: '#9F9F9F',
     textStrong: '#212121',
+
+    bottomBarTint: '#FA8057',
+    bottomBarActive: '#FFFFFF',
+    bottomBarInactive: '#FA8057',
+
+    buttonBackground: '#FA8057',
+    buttonText: '#FAFAFA',
+
+    inputBackground: '#F7F9FC',
+    inputBorder: '#EEF2F7',
+    inputPlaceholder: '#737985',
+
+    success: '#44D775',
+    error: '#ff4747',
   },
-  gradients: {},
+  gradients: {
+    primary: ['#FA8057', '#F94D1D', '#F94D1D', '#F94D1D', '#FA8057'],
+  },
 };
 
 export const DarkThemeColors = {
   colors: {
     transparent: 'transparent',
-    primary: '#8775FF',
-    accent: '#FFBE28',
+
+    primary: '#FA8057',
     background: '#242424',
-    backgroundSecondary: '#272727',
-    success: '#74D0AC',
+    backgroundSecondary: '#3A3A3A',
+    surface: '#272727',
+
+    text: '#FAFAFA',
+    textLight: '#A0A0A0',
+    textStrong: '#DEDEDE',
+
+    bottomBarTint: '#FA8057',
+    bottomBarActive: '#FFFFFF',
+    bottomBarInactive: '#FA8057',
+
+    buttonBackground: '#FA8057',
+    buttonText: '#FAFAFA',
+
+    inputBackground: '#272727',
+    inputBorder: '#3D3D3D',
+    inputPlaceholder: '#BABABA',
+
+    success: '#44D775',
     error: '#ff4747',
-    text: '#dbdbdb',
-    textLight: '#d8d8d8',
-    textStrong: '#dedede',
   },
-  gradients: {},
+  gradients: {
+    primary: ['#FA8057', '#F94D1D', '#F94D1D', '#F94D1D', '#FA8057'],
+  },
 };
