8f6ff4c73653c685c45473985c21b9990f497931
Uğur Eren <contact@ugureren.net>
2023-04-27 20:31:39 +0300

    Add Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import {useCallback} from 'react';
 import {View} from 'react-native';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import {useTheme} from './Hooks';
 import Providers from './Providers';
 import Router from './Router';
 import Env from './Utils/Env';
 import {ThemedStyleSheet} from './Utils/ThemedStyleSheet';
+
+SplashScreen.preventAutoHideAsync();
 
 function App() {
   if (!Env.USE_CONFIG || Env.USE_CONFIG !== 'true') {
@@ -24,8 +29,23 @@ const AppContent: React.FC = () => {
 
   const styles = getStyles(theme);
 
+  const [fontsLoaded] = useFonts({
+    'FiraSans-Regular': require('./Assets/Fonts/FiraSans/FiraSans-Regular.ttf'),
+    'FiraSans-Medium': require('./Assets/Fonts/FiraSans/FiraSans-Medium.ttf'),
+    'FiraSans-SemiBold': require('./Assets/Fonts/FiraSans/FiraSans-SemiBold.ttf'),
+    'FiraSans-Bold': require('./Assets/Fonts/FiraSans/FiraSans-Bold.ttf'),
+
+    'EarWorm-Demo': require('./Assets/Fonts/EarWorm/EarWorm-Demo.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Router />
     </View>
   );
