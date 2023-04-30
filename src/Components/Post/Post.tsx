022832eaac3fc683d9a01a0ddd054645950a8e84
Uğur Eren <contact@ugureren.net>
2023-04-30 21:37:48 +0300

    Use UserInfo and PostContent in Post component

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -1,8 +1,10 @@
 import {View} from 'react-native';
 import {Divider} from 'react-native-paper';
 import Text from '../Text/Text';
+import UserInfo from '../UserInfo/UserInfo';
 import TextButton from '../TextButton/TextButton';
 import Touchable from '../Touchable/Touchable';
+import PostContent from '../PostContent/PostContent';
 import {useTheme} from '../../Hooks';
 import getStyles from './Post.styles';
 
@@ -13,11 +15,38 @@ const Post: React.FC = () => {
 
   return (
     <View style={styles.container}>
+      <UserInfo timestamp={1682794718492} />
+
       <Text style={styles.description}>
         Eiusmod laborum adipisicing do mollit voluptate exercitation esse. Pariatur amet anim ex
         velit. Tempor elit velit sit fugiat ad duis commodo veniam eu. Proident consectetur
         voluptate sint irure id et ipsum officia ea est ex sint in.
       </Text>
+
+      <PostContent
+        data={[
+          {
+            type: 'image',
+            ratio: 1,
+            uri: `https://unsplash.it/600/600/?random=${Math.random()}`,
+          },
+          {
+            type: 'video',
+            ratio: 1,
+            uri: 'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          },
+          {
+            type: 'image',
+            ratio: 1.5,
+            uri: `https://unsplash.it/900/600/?random=${Math.random()}`,
+          },
+          {
+            type: 'image',
+            ratio: 1,
+            uri: `https://unsplash.it/600/600/?random=${Math.random()}`,
+          },
+        ]}
+      />
 
       <View style={styles.bottom}>
         <View style={styles.actionButtons}>
