8ee4dc8e3d2dae2e156567c00771d1cbecd12014
Uğur Eren <contact@ugureren.net>
2023-04-30 23:47:41 +0300

    Use CircleProgress component in PostImage

--- src/Components/PostMedia/PostImage/PostImage.styles.ts
+++ src/Components/PostMedia/PostImage/PostImage.styles.ts
@@ -1,9 +1,18 @@
+import Color from 'color';
 import {StyleSheet} from 'react-native';
+import {Colors} from '../../../Styles';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  loader: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Color(Colors.CommonColors.black).alpha(0.25).toString(),
   },
 });
--- src/Components/PostMedia/PostImage/PostImage.tsx
+++ src/Components/PostMedia/PostImage/PostImage.tsx
@@ -1,12 +1,18 @@
+import {useState} from 'react';
 import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import {Image, ImageStyle} from 'expo-image';
+import {useSharedValue} from 'react-native-reanimated';
 import {PostImageProps} from './PostImage.props';
 import styles from './PostImage.styles';
+import CircleProgress from '../../CircleProgress/CircleProgress';
 
 const PostImage: React.FC<PostImageProps> = (props) => {
   const {uri, ratio, thumbnail, style, ...imageProps} = props;
 
   const {width} = useWindowDimensions();
+
+  const [loading, setLoading] = useState(true);
+  const progress = useSharedValue(0);
 
   return (
     <View style={styles.container}>
@@ -15,11 +21,25 @@ const PostImage: React.FC<PostImageProps> = (props) => {
         placeholder={thumbnail}
         transition={500}
         contentFit="contain"
+        onProgress={(e) => {
+          progress.value = e.loaded / e.total;
+        }}
+        onLoad={() => setLoading(false)}
         style={StyleSheet.compose(style, {width, height: width / ratio}) as ImageStyle}
         {...imageProps}
       />
 
-      {/* TODO: Add loading indicator */}
+      {loading ? (
+        <View style={styles.loader}>
+          <CircleProgress
+            width={100}
+            height={100}
+            progress={progress}
+            radius={50}
+            strokeWidth={5}
+          />
+        </View>
+      ) : null}
     </View>
   );
 };
