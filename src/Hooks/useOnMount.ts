4bd9d23059b0f1d43d9e02951fb04a5b689d7723
Uğur Eren <contact@ugureren.net>
2023-05-13 22:36:12 +0300

    Add new eslint rules

--- src/Hooks/useOnMount.ts
+++ src/Hooks/useOnMount.ts
@@ -1,5 +1,6 @@
 import {useEffect, EffectCallback} from 'react';
 
 export const useOnMount = (effect: EffectCallback): void => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return useEffect(effect, []);
 };
