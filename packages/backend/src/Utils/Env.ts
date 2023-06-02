16374029c8554c9795ddb6ddbca9d56ebe9256de
Uğur Eren <contact@ugureren.net>
2023-06-02 12:28:21 +0300

    Use single env file

--- packages/backend/src/Utils/Env.ts
+++ packages/backend/src/Utils/Env.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line prefer-destructuring
-const env = process.env;
+const env = (key: string) => process.env[`SVEJ_BACKEND_${key}`];
 
-export const PORT = env.PORT || 3001;
-export const BCRYPT_ROUNDS = env.BCRYPT_ROUNDS || 10;
+export const USE_CONFIG = env('USE_CONFIG') || 'false';
+export const PORT = Number(env('PORT')) || 3001;
+export const BCRYPT_ROUNDS = Number(env('BCRYPT_ROUNDS')) || 10;
