373ce672168aaf1869061ceb284b4837fa5218ae
Uğur Eren <contact@ugureren.net>
2024-02-11 18:32:37 +0300

    Move onlyAuthorized logic to JWT verify function

--- packages/backend/src/Middlewares/Authentication.ts
+++ packages/backend/src/Middlewares/Authentication.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import {JWT} from 'server-side';
 import {HTTPStatus, ErrorCodes} from 'common';
 import {User} from 'database';
-import {Prisma} from '../Services';
 
 type Locals = {
   authenticated?: boolean;
@@ -28,37 +27,17 @@ export const onlyAuthorized = async (
   const result = await JWT.verify(token);
 
   if (!result.ok) {
-    res
-      .status(HTTPStatus.Unauthorized)
-      .send({code: ErrorCodes.InvalidAuthToken, error: result.error});
-    return;
-  }
-
-  if (typeof result.decoded !== 'object' || !result.decoded.sub) {
-    res.status(HTTPStatus.Unauthorized).send({code: ErrorCodes.InvalidAuthToken});
-    return;
-  }
-
-  const {decoded} = result;
-
-  // `Making database calls while validating JWT tokens undermines their primary advantage of being stateless.`
-  // But this is a simple example backend, not going to be a production-ready one. So we can ignore this for now.
-  const user = await Prisma.user.findUnique({
-    where: {
-      id: decoded.sub,
-    },
-  });
-
-  // Using jti whitelist instead of blacklist
-  if (!user || !user.id || !user.active || !decoded.jti || !user.jtis.includes(decoded.jti)) {
-    res.status(HTTPStatus.Unauthorized).send({code: ErrorCodes.Unauthorized});
+    res.status(HTTPStatus.Unauthorized).send({
+      code: result.cause === 'Unauthorized' ? ErrorCodes.Unauthorized : ErrorCodes.InvalidAuthToken,
+      error: result.error,
+    });
     return;
   }
 
   res.locals.authenticated = true;
-  res.locals.decoded = decoded;
+  res.locals.decoded = result.decoded;
   res.locals.token = token;
-  res.locals.user = user;
+  res.locals.user = result.user;
 
   next();
 };
--- packages/server-side/src/JWT.ts
+++ packages/server-side/src/JWT.ts
@@ -1,5 +1,7 @@
+import {User} from 'database';
 import JWT from 'jsonwebtoken';
 import {JWT_EXPIRE_TIME, JWT_PRIV_KEY} from './Env';
+import PrismaClient from './Prisma';
 
 export type JwtPayload = JWT.JwtPayload;
 export type VerifyErrors = JWT.VerifyErrors;
@@ -17,11 +19,13 @@ export type SignReturnType =
 export type VerifyReturnType =
   | {
       ok: true;
-      decoded: JWT.JwtPayload | string;
+      user: User;
+      decoded: JWT.JwtPayload;
     }
   | {
       ok: false;
-      error: JWT.VerifyErrors;
+      cause?: 'Unauthorized' | 'InvalidAuthToken';
+      error?: JWT.VerifyErrors;
     };
 
 export const sign = async (payload: JWT.JwtPayload): Promise<SignReturnType> => {
@@ -51,13 +55,35 @@ export const sign = async (payload: JWT.JwtPayload): Promise<SignReturnType> => 
 export const verify = async (token: string): Promise<VerifyReturnType> => {
   return new Promise((resolve) => {
     JWT.verify(token, JWT_PRIV_KEY, (err, decoded) => {
-      if (err || !decoded) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        resolve({ok: false, error: err!});
+      if (err) {
+        resolve({ok: false, error: err || undefined});
         return;
       }
 
-      resolve({ok: true, decoded});
+      if (typeof decoded !== 'object' || !decoded.sub) {
+        resolve({ok: false, cause: 'InvalidAuthToken'});
+        return;
+      }
+
+      // `Making database calls while validating JWT tokens undermines their primary advantage of being stateless.`
+      // But this is a simple example backend, not going to be a production-ready one. So we can ignore this for now.
+      PrismaClient.user
+        .findUnique({
+          where: {
+            id: decoded.sub,
+          },
+        })
+        .then((user) => {
+          if (!user || !user.id || !decoded.jti || !user.jtis.includes(decoded.jti)) {
+            resolve({ok: false, cause: 'Unauthorized'});
+            return;
+          }
+
+          resolve({ok: true, user, decoded});
+        })
+        .catch(() => {
+          resolve({ok: false, cause: 'Unauthorized'});
+        });
     });
   });
 };
